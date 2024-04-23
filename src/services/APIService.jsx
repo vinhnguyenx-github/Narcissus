491eead131423b2ab4a4e02af80a99968348d201
Vinh <vinh.nguyen09102003@gmail.com>
2024-04-23 06:54:05 -0700

    update api

--- src/services/APIService.jsx
+++ src/services/APIService.jsx
@@ -1,35 +1,37 @@
 import axios from "axios";
 
-const ROOM_URL = "http://localhost:8080/api/roomType";
-
-const GALLERY_URL = "http://localhost:8080/api/gallery";
+const BASE_URL = "https://narcissus-backend.de.r.appspot.com/api";
 
 function fixImagePath(imagePath) {
   return imagePath.replace(/\\/g, "/");
 }
 
 class APIService {
+  //Booking
+  booking(data) {
+    return axios.post(`${BASE_URL}/booking/book`, data);
+  }
   // Room
   getTypes() {
-    return axios.get(`${ROOM_URL}/find`);
+    return axios.get(`${BASE_URL}/roomType/find`);
   }
   getImageUrl(id) {
-    return axios.get(`${ROOM_URL}/getImage/${id}`);
+    return axios.get(`${BASE_URL}/roomType/getImage/${id}`);
   }
   getHotel(id) {
-    return axios.get(`${ROOM_URL}/findHotel/${id}`);
+    return axios.get(`${BASE_URL}/roomType/findHotel/${id}`);
   }
   // Gallery
   getAllImages() {
-    return axios.get(`${GALLERY_URL}/find`);
+    return axios.get(`${BASE_URL}/gallery/find`);
   }
   getImageByRoom(id) {
-    return axios.get(`${GALLERY_URL}/find/${id}`);
+    return axios.get(`${BASE_URL}/gallery/find/${id}`);
   }
   getImage(imageURL, format) {
     const fixedImagePath = fixImagePath(imageURL);
     return axios.get(
-      `${GALLERY_URL}/getImage?url=${fixedImagePath}&format=${format}`,
+      `${BASE_URL}/gallery/getImage?url=${fixedImagePath}&format=${format}`,
       { responseType: "arraybuffer" }
     );
   }
