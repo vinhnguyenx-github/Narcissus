4c520ad1ccbac2860d52db461bc5d23fd9ef9300
Vinh <vinh.nguyen09102003@gmail.com>
2024-04-25 03:13:09 -0700

    test

--- src/pages/Location1/pages/RoomTypeBooking.jsx
+++ src/pages/Location1/pages/RoomTypeBooking.jsx
@@ -4,22 +4,8 @@ import RoomTypeBooking from "../../../components/Location/RoomTypeBooking/RoomTy
 import Footer from "../../../components/Footer/Footer";
 import BubbleChat from "../../../components/BubbleChat/BubbleChat";
 import AdditionalInfo from "../../../components/Location/AdditionalInfo/AdditionalInfo";
-import { useNavigate, useParams } from "react-router-dom";
-import { useRoomData } from "../../../provider/roomtype/roomTypeProvider";
 
 const RoomTypeBooking1 = () => {
-  const { type } = useParams();
-  const { rooms } = useRoomData();
-  const navigate = useNavigate();
-
-  const roomExists = rooms.some((room) => room.type === type);
-
-  if (!roomExists) {
-    // Redirect to the /rooms route if the room type doesn't exist
-    navigate("/33-tuc-mac/rooms");
-    return null; // Return null to prevent rendering anything for this case
-  }
-
   return (
     <div>
       <LocationNav />
--- src/pages/Location2/pages/RoomTypeBooking.jsx
+++ src/pages/Location2/pages/RoomTypeBooking.jsx
@@ -1,22 +1,10 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { useRoomData } from "../../../provider/roomtype/roomTypeProvider";
 import LocationNav from "../../../components/Location/LocationNav/LocationNav";
 import RoomTypeBooking from "../../../components/Location/RoomTypeBooking/RoomTypeBooking";
 import BubbleChat from "../../../components/BubbleChat/BubbleChat";
 import Footer from "../../../components/Footer/Footer";
 
 const RoomTypeBooking2 = () => {
-  const { type } = useParams();
-  const { rooms } = useRoomData();
-  const navigate = useNavigate();
-
-  if (!rooms) {
-    // Redirect to the /rooms route if the room type doesn't exist
-    navigate("/84-le-duan/rooms");
-    return null; // Return null to prevent rendering anything for this case
-  } // Access the room type from the route parameter
-
   return (
     <div>
       <LocationNav />
